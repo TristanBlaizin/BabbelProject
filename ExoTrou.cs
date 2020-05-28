9ef890f00ca46a6d89a49bd7dcba0caf52ab4190
TristanBlaizin <tristan.blaizin@gmail.com>
2020-05-28 17:31:53 +0200

    Exo trou modif

--- ExoTrou.cs
+++ ExoTrou.cs
@@ -28,7 +28,35 @@ namespace BabbelProject
         }
         private void ExoTrou_Load(object sender, EventArgs e)
         {
+            string str = string.Empty;
+            foreach (Object item in InfosExo.ItemArray)
+            {
+                str += item.ToString() + " ";
+            }
 
+            lblTrouConsigne.Text = InfosExo.ItemArray[3].ToString();
+            DataRow phraseComplete = Babbel.Tables["Phrases"].Select($"codePhrase = '{InfosExo.ItemArray[5]}'")[0];
+            DataRow motsPerdu = Babbel.Tables["Exercices"].Select($"listeMots = '{InfosExo.ItemArray[7]}'")[0];
+            label1.Text = phraseComplete.ItemArray[1].ToString();
+            string[] tabMot = phraseComplete.ItemArray[1].ToString().Split(' ') ;
+            string[] tabMotPerdu = motsPerdu.ItemArray[7].ToString().Split('/');
+            string[] tabMotsPhraseATrou = new string[tabMot.Length];
+
+            for (int i = 0; i < tabMot.Length; ++i)
+            {
+                for (int y = 0;y < tabMotPerdu.Length; y++)
+                {
+                    if ( i == Int64.Parse(tabMotPerdu[y])-1)
+                    {
+
+                    }
+                    else {
+                        label2.Text += tabMot[i] +" ";
+
+                    }
+                }
+            }
+          
         }
     }
 }
